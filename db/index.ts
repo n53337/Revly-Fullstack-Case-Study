30a76c582c7277a89ad9b4aeb0011ca9a53e207f
Youssef <scalewithusef@gmail.com>
2025-07-31 23:40:05 +0100

    feat: switched to turso to simulate the remote db connection

--- db/index.ts
+++ db/index.ts
@@ -1,9 +1,12 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/libsql';
-import { createClient } from '@libsql/client';
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
 import * as schema from "@/db/schema";
 
-const client = createClient({ url: process.env.DB_FILE_NAME! });
+const client = createClient({
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
+});
 const db = drizzle({ client, schema });
 
 export default db;
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,12 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './db/schema.ts',
-  dialect: 'sqlite',
+  out: "./drizzle",
+  schema: "./db/schema.ts",
+  dialect: "turso",
   dbCredentials: {
-    url: process.env.DB_FILE_NAME!,
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
   },
 });
